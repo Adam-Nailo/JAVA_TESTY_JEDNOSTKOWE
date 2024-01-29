a5c1c2363f80dc2b0001c3af16eff9313d25626e
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-29 21:07:49 +0100

    Istniejący projekt w Gradle

--- TestyJUnitGradle/TestyMiernikaPradu/src/test/java/org/projekt_testy_junit/ExampleTest.java
+++ TestyJUnitGradle/TestyMiernikaPradu/src/test/java/org/projekt_testy_junit/ExampleTest.java
@@ -1,10 +1,16 @@
 package org.projekt_testy_junit;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by Adam Seweryn
  */
 class ExampleTest {
+    @Test
+    public void example() {
+
+    }
 
 }
