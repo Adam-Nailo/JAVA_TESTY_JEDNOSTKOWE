1a3764515c8726f19cdb1e2b7a6f741ba17fc8f9
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-29 20:49:50 +0100

    Adam Seweryn Szkolenia Egzaminy Ulubione Notatki Certyfikaty Postępy Profil Wróć do sklepu Wyloguj się Kurs JAVA - testy jednostkowe Nowy projekt w Maven

---
+++ TestyJUnitMaven/src/main/java/org/projekt_testy_junit/App.java
@@ -0,0 +1,13 @@
+package org.projekt_testy_junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
